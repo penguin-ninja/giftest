c8fec75dc6c8f3aa5febd70839443a440c26bbb2
Joao Silva <joaokha713@gmail.com>
2017-08-02 09:06:31 +0100

    chore(app): change url loader config for safari

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -71,7 +71,7 @@ module.exports = {
         loader: 'babel-loader',
       }, {
         test: /\.(jpe?g|gif|png|svg|woff|woff2|ttf|eot)$/i,
-        loader: 'url-loader?limit=10000',
+        loader: 'url-loader?limit=10',
       }, {
         test: /\.json$/,
         loader: 'json-loader',
