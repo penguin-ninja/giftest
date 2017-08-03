00917c8553841b218c89e5f6723fda0beecc9b57
Joao Silva <joaokha713@gmail.com>
2017-08-03 14:39:59 +0100

    chore(app): added tag manager code

--- server/server.js
+++ server/server.js
@@ -98,8 +98,19 @@ const renderFullPage = (html, initialState) => {
         ${head.script.toString()}
         ${process.env.NODE_ENV === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
         <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PJQZHXH');</script>
+        <!-- End Google Tag Manager -->
       </head>
       <body class="app">
+          <!-- Google Tag Manager (noscript) -->
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJQZHXH"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <!-- End Google Tag Manager (noscript) -->
         <script>
           window.fbAsyncInit = function() {
             FB.init({
