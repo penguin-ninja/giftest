ddf5f106eb5fea79cdb93c9a9ebe32fdb8881177
Joao Silva <joaokha713@gmail.com>
2017-09-12 16:14:08 +0100

    feat(config): update quantize to 128

--- server/config.js
+++ server/config.js
@@ -69,7 +69,7 @@ const config = {
     outputParams: {
       frameCount: '10',
       frameDelay_ms: '500',
-      quantizeImage: '64',
+      quantizeImage: '128',
     },
     custImg2_effectA_param: {
       op: 'faceDetect',
